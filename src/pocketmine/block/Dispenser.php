5066c4d5f821338de84bfb1030bd20f97c8af349
BlackShadow1 <marketto_02@hotmail.it>
2016-01-28 23:11:19 +0100

    Update Dispenser.php

--- src/pocketmine/block/Dispenser.php
+++ src/pocketmine/block/Dispenser.php
@@ -26,7 +26,16 @@
 
 namespace pocketmine\block;
 
-use pocketmine/Item/Item;
+use pocketmine\item\Item;
+use pocketmine\item\Tool;
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\tag\Compound;
+use pocketmine\nbt\tag\Enum;
+use pocketmine\nbt\tag\Int;
+use pocketmine\nbt\tag\String;
+use pocketmine\Player;
+use pocketmine\tile\Furnace;
+use pocketmine\tile\Tile;
 
 class Dispenser extends Solid{
 
@@ -40,6 +49,76 @@ class Dispenser extends Solid{
 		return "Dispenser";
 	}
 
-        public function getDrops(Item $item){
-                return [Item::get(Item::DISPENSER), 0, 1];
+        public function canBeActivated(){
+		return true;
+	}
+	public function getHardness(){
+		return 3.5;
+	}
+
+        public function place(Item $item, Block $block, Block $target, $fx, $fy, $fz, Player $player = null){
+            $faces = [
+			0 => 4,
+			1 => 2,
+			2 => 5,
+			3 => 3,
+		];
+		$this->meta = $faces[$player instanceof Player ? $player->getDirection() : 0];
+		$this->getLevel()->setBlock($block, $this, true, true);
+		$nbt = new Compound("", [
+			new Enum("Items", []),
+			new String("id", Tile::DISPENSER),
+			new Int("x", $this->x),
+			new Int("y", $this->y),
+			new Int("z", $this->z)
+		]);
+		$nbt->Items->setTagType(NBT::TAG_Compound);
+		if($item->hasCustomName()){
+			$nbt->CustomName = new String("CustomName", $item->getCustomName());
+		}
+		if($item->hasCustomBlockData()){
+			foreach($item->getCustomBlockData() as $key => $v){
+				$nbt->{$key} = $v;
+			}
+		}
+		Tile::createTile("Dispenser", $this->getLevel()->getChunk($this->x >> 4, $this->z >> 4), $nbt);
+		return true;
+	}
+	public function onActivate(Item $item, Player $player = null){
+		if($player instanceof Player){
+			$t = $this->getLevel()->getTile($this);
+			$dispenser = false;
+			if($t instanceof Dispenser){
+				$dispeser = $t;
+			}else{
+				$nbt = new Compound("", [
+					new Enum("Items", []),
+					new String("id", Tile::DISPENSER),
+					new Int("x", $this->x),
+					new Int("y", $this->y),
+					new Int("z", $this->z)
+				]);
+				$nbt->Items->setTagType(NBT::TAG_Compound);
+				$furnace = Tile::createTile("Dispenser", $this->getLevel()->getChunk($this->x >> 4, $this->z >> 4), $nbt);
+			}
+			if(isset($dispenser->namedtag->Lock) and $dispenser->namedtag->Lock instanceof String){
+				if($dispenser->namedtag->Lock->getValue() !== $item->getCustomName()){
+					return true;
+				}
+			}
+			if($player->isCreative()){
+				return true;
+			}
+			$player->addWindow($dispenser->getInventory());
+		}
+		return true;
+	}       
+    }
+    public function getDrops(Item $item){
+		$drops = [];
+		if($item->isPickaxe() >= 1){
+			$drops[] = [Item::DISPENSER, 0, 1];
+		}
+		return $drops;
+	}
 }
