6a0dad5cca105e5e199deecaf296bac6a52f5211
Ronald Schmidt <ronald.schmidt@zu-web.de>
2016-01-29 23:03:11 +0100

    update minecart entity, taken from boat entity

--- src/pocketmine/entity/Minecart.php
+++ src/pocketmine/entity/Minecart.php
@@ -23,6 +23,13 @@
  *
 */
 namespace pocketmine\entity;
+
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\network\protocol\EntityEventPacket;
+use pocketmine\item\Item as ItemItem;
+
 class Minecart extends Vehicle{
      const NETWORK_ID = 84;
     /**
@@ -34,7 +41,56 @@ class Minecart extends Vehicle{
     public function initEntity(){
         parent::initEntity();
     }
+
     public function getName(){
         return "Minecart";
+    }
+
+    public function spawnTo(Player $player){
+        $pk = new AddEntityPacket();
+        $pk->eid = $this->getId();
+        $pk->type = Boat::NETWORK_ID;
+        $pk->x = $this->x;
+        $pk->y = $this->y;
+        $pk->z = $this->z;
+        $pk->speedX = 0;
+        $pk->speedY = 0;
+        $pk->speedZ = 0;
+        $pk->yaw = 0;
+        $pk->pitch = 0;
+        $pk->metadata = $this->dataProperties;
+        $player->dataPacket($pk);
+
+        parent::spawnTo($player);
+    }
+
+    public function attack($damage, EntityDamageEvent $source){
+        parent::attack($damage, $source);
+
+        if(!$source->isCancelled()){
+            $pk = new EntityEventPacket();
+            $pk->eid = $this->id;
+            $pk->event = EntityEventPacket::HURT_ANIMATION;
+            foreach($this->getLevel()->getPlayers() as $player){
+                $player->dataPacket($pk);
+            }
+        }
+    }
+
+    public function kill(){
+        parent::kill();
+
+        foreach($this->getDrops() as $item){
+            $this->getLevel()->dropItem($this, $item);
+        }
+    }
+
+    public function getDrops(){
+        return [ItemItem::get(ItemItem::Minecart, 0, 1)];
+    }
+
+    public function getSaveId(){
+        $class = new \ReflectionClass(static::class);
+        return $class->getShortName();
     }
 }
