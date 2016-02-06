12ca9cff543b7336babb8ae0cda70ffd7693ec12
Keith @ ImagicalMine <keithkfng@users.noreply.github.com>
2016-02-06 20:54:01 +1100

    Update JenkinsBuildStart.sh

--- tests/JenkinsBuildStart.sh
+++ tests/JenkinsBuildStart.sh
@@ -1,4 +1,5 @@
 tar zxf /etc/php5-pm/PHP_5.6.10_x86-64_Linux.tar.gz
+cd ..
 if [ ! -d "plugins" ]; then  
 mkdir "plugins"  
 fi
@@ -6,5 +7,6 @@ if [ ! -d "releases" ]; then
 mkdir "releases"  
 fi
 cp /etc/php5-pm/DevTools_v1.10.0.phar ./plugins/DevTools.phar
+cd tests
 chmod +x ./JenkinsBuild.php
 ./JenkinsBuild.php
