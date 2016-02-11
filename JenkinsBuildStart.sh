9a1fc7d363f93dd7612bc6505e3d15f28acf3ef3
Keith @ ImagicalMine <keithkfng@users.noreply.github.com>
2016-02-11 18:51:23 +1100

    Fix up Jenkins

--- JenkinsBuildStart.sh
+++ JenkinsBuildStart.sh
@@ -1,10 +1,11 @@
-tar zxf /etc/php5-pm/PHP_5.6.10_x86-64_Linux.tar.gz
+wget https://bintray.com/artifact/download/pocketmine/PocketMine/PHP_5.6.10_x86-64_Linux.tar.gz
+tar zxf PHP_5.6.10_x86-64_Linux.tar.gz
 if [ ! -d "plugins" ]; then  
 mkdir "plugins"  
 fi
 if [ ! -d "releases" ]; then  
 mkdir "releases"  
 fi
-cp /etc/php5-pm/DevTools_v1.10.0.phar ./plugins/DevTools.phar
+wget -O plugins/DevTools.phar https://github.com/PocketMine/DevTools/releases/download/v1.9.0/DevTools_v1.9.0.phar
 chmod +x ./JenkinsBuild.php
 ./JenkinsBuild.php ${BUILD_NUMBER}
