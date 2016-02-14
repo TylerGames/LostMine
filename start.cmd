4206eebb91d3d661b6d1d1656631d3d0be7d9bd7
MCUnderground <daniel.barinak@gmail.com>
2016-02-14 13:18:31 +0100

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,29 +1,12 @@
 @echo off
-::
-::  _                       _           _ __  __ _             
-:: (_)                     (_)         | |  \/  (_)            
-::  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___  
-:: | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \ 
-:: | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/ 
-:: |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___| 
-::                     __/ |                                   
-::                    |___/                                                                     
-:: 
-:: This program is a third party build by ImagicalMine.
-::
-:: ImagicalMine is free software: you can redistribute it and/or modify
-:: it under the terms of the GNU Lesser General Public License as published by
-:: the Free Software Foundation, either version 3 of the License, or
-:: (at your option) any later version.
-::
-:: @author PocketMine Team, edited by the ImagicalMine team
-:: @link http://imagicalmine.net
-::
-
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: ERROR: Restricted area - access denied. Don't try to edit anything below this line! ::
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
+/*
+ _              _  _____                    
+| |    ___  ___| ||_   _|__  __ _ _ __ ___  
+| |   / _ \/ __| __|| |/ _ \/ _` | '_ ` _ \ 
+| |__| (_) \__ \ |_ | |  __/ (_| | | | | | |
+|_____\___/|___/\__||_|\___|\__,_|_| |_| |_|
+ */  
+   
 TITLE LostTeam server software for Minecraft: Pocket Edition
 cd /d %~dp0
 
